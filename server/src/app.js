214202b8f509bae165fb68c928d34201d8aa5c74
Ihor Ivliev <igor852745@gmail.com>
2020-03-26 13:18:23 +0200

    ws server commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,18 @@
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
+const connectionHandler = require('./ws');
+
+const router = require('./router');
+
+app.use(router);
+
+io.on('connection', connectionHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`));
+server.listen(PORT,
+    () => console.log(`Example app listening on port ${ PORT }!`));
---
+++ server/src/ws/index.js
@@ -0,0 +1,13 @@
+
+const {testHandler, messageHandler} = require('./eventHandlers');
+module.exports = function connectionHandler(socket) {
+
+    /*	const handshake = socket.handshake;
+     console.log('handshake');
+     console.dir(handshake);*/
+
+    socket.on('test', testHandler);
+
+    socket.on('message', messageHandler);
+
+};
